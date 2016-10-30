d35be3a8d686a165e50c3b187d074379914c4710
martin vert <mart1ver@gmail.com>
2016-10-30 16:37:05 +0100

    commencons par l'interface d'ajout des chantiers

--- ifaces/index.php
+++ ifaces/index.php
@@ -166,21 +166,10 @@ var projectionSelect = document.getElementById('projection');
     </script>
 
 
+<?php include "pied.php";?>
 
 
 
 
 
 
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="../js/bootstrap-switch.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
-    <script src="../js/docs.min.js"></script>
-
-  </body>
-</html>
