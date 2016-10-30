cf0137051c255ad6aa7fff7bd74124e21ba94189
martin vert <mart1ver@gmail.com>
2016-10-30 17:23:53 +0100

    commencons par l'interface d'ajout des chantiers

--- ifaces/index.php
+++ ifaces/index.php
@@ -104,8 +104,8 @@ var mousePositionControl = new ol.control.MousePosition({
 
       var draw; // global so we can remove it later
       function addInteraction() {
-        var value = typeSelect.value;
-        if (value !== 'None') {
+        var value = pose.checked;
+        if (value !== false) {
           var geometryFunction, maxPoints;
          
           draw = new ol.interaction.Draw({
@@ -122,7 +122,7 @@ var mousePositionControl = new ol.control.MousePosition({
       /**
        * Handle change event.
        */
-      typeSelect.onchange = function() {
+      pose.onchange = function() {
         map.removeInteraction(draw);
         addInteraction();
       };
