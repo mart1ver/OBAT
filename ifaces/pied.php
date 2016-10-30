54653c934dfe3fa0d190e154cacd13805ff5b7be
martin vert <mart1ver@gmail.com>
2016-10-30 17:46:51 +0100

    commencons par l'interface d'ajout des chantiers

--- ifaces/pied.php
+++ ifaces/pied.php
@@ -1,6 +1,4 @@
-<script src="../js/jquery-3.1.1.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
-     <script src="../js/bootstrap-switch.min.js"></script>
+
 <div class="container">
   <footer>
         <p class="text-left">Obat. 2016</p>
