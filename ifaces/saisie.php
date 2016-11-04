1c2c3d710fdb0c383943d0fb3a187d3768c35c30
martin vert <mart1ver@gmail.com>
2016-11-04 19:49:22 +0100

    import et creation des folios

--- ifaces/saisie.php
+++ ifaces/saisie.php
@@ -6,7 +6,16 @@ include "tete.php" ?>
 
 
 <br><br>
-<h2>Chantier N° <?php echo($_GET["id"]); ?>, Folio N°<?php echo($_GET["fid"]); ?></h2>
+<h2>Chantier N° <?php echo($_GET["id"]); ?>, Folio N°<div class="btn-group">
+  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Action
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="#">1</a>
+    <a class="dropdown-item" href="#">2</a>
+    <a class="dropdown-item" href="#">3</a>
+      </div>
+</div></h2>
 
 <label>Poser des points
 <input type="checkbox" id="pose" name="pose" checked>
