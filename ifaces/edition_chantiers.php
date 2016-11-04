3e7c81b8347fecb59883da38aa2a04ad9a75165d
martin vert <mart1ver@gmail.com>
2016-11-04 18:13:51 +0100

    import et creation des folios

--- ifaces/edition_chantiers.php
+++ ifaces/edition_chantiers.php
@@ -67,6 +67,106 @@ require_once('../moteur/dbconfig.php');
 </div>
       </div>
      
+
+  <!-- Table -->
+      <table class="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Date de création</th>
+            <th>Nom</th>
+            <th>Commentaire</th>
+            <th>Nombre de folios</th>
+            <th>Visible?</th>
+            <th>Éditer</th>
+          </tr>
+        </thead>
+        <tbody>
+        <?php 
+            // On recupère tout le contenu de la table chantiers
+            $reponse = $bdd->query('SELECT * FROM chantiers');
+ 
+           // On affiche chaque entree une à une
+           while ($donnees = $reponse->fetch())
+           {
+
+           ?>
+            <tr> 
+            <td><?php echo $donnees['id']?></td>
+            <td><?php echo $donnees['timestamp']?></td>
+            <td><?php echo $donnees['nom']?></td>
+            <td><?php echo $donnees['adresse']?></td>
+            <td><?php echo $donnees['commentaire']?></td>
+        <td>   
+<form action="../moteur/chantier_visible.php" method="post">
+
+  <input type="hidden" name ="id" id="id" value="<?php echo $donnees['id']?>">
+  <input type="hidden"name ="visible" id ="visible" value="<?php if ($donnees['visible'] == "oui") 
+{echo "non";}
+else 
+{echo "oui";}?>">
+<?php
+if ($donnees['visible'] == "oui") // SI visible == oui
+{?>
+ <button  class="btn btn-info btn-sm " >
+  <?php
+}
+
+else // SINON 
+{?>
+   <button  class="btn btn-danger btn-sm " >
+ <?php
+}
+ echo $donnees['visible']?> 
+  </button>
+
+</form>
+</td>
+
+
+
+
+<td>
+
+<form action="edition_chantiers.php" method="post">
+
+<input type="hidden" name ="id" id="id" value="<?php echo $donnees['id']?>">
+<input type="hidden" name ="nom" id="nom" value="<?php echo $donnees['nom']?>">
+<input type="hidden" name ="adresse" id="adresse" value="<?php echo $donnees['adresse']?>">
+<input type="hidden" name ="commentaire" id="commentaire" value="<?php echo $donnees['commentaire']?>">
+  <button  class="btn btn-warning btn-sm" >Modifier</button>
+
+
+</form>
+
+
+
+</td>
+
+
+
+
+
+
+          </tr>
+           <?php }
+              $reponse->closeCursor(); // Termine le traitement de la requête
+                ?>
+       </tbody>
+        <tfoot>
+          <tr>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+           
+            
+          </tfoot>
+        
+      </table>
+     
       <br>
       <div class="row">
   <div class="col-md-4"></div>
