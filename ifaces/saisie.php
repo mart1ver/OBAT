340f9767f50c10817970a6c1119d75fb8bad0095
martin vert <mart1ver@gmail.com>
2016-11-05 20:27:39 +0100

    saisie de spots premier jet

--- ifaces/saisie.php
+++ ifaces/saisie.php
@@ -210,7 +210,7 @@ var mousePositionControl = new ol.control.MousePosition({
       }
 
 function spot_add() {
-  document.getElementById('coordos').value = document.getElementById('mouse-position').innerHTML;
+  document.getElementById('coordos').value = document.getElementsByClassName('custom-mouse-position').innerHTML;
   $('#myModal').modal();
 }
 
