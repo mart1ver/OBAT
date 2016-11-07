f3dc9fed108abb1b6f0a8c3889448ed4218c797a
martin vert <mart1ver@gmail.com>
2016-11-07 22:29:34 +0100

    saisie des spots

--- ifaces/saisie.php
+++ ifaces/saisie.php
@@ -109,37 +109,37 @@ Chantier N° <?php echo($_GET["id"]); ?>: <?php echo($nom_chantier); ?>  , Folio
       </div>
       <div class="modal-body">
         <input type="hidden" id="coordos"> 
-        <label for="bat">localisation:</label><input type="text" name="bat" id="bat" placeholder="Bat." maxlength="6" size="6" ><input type="text" name="entree" id="entree" placeholder="Entrée" maxlength="6" size="6"><input type="text" name="cage" id="cage" placeholder="Cage" maxlength="6" size="6" ><input type="text" name="etage" id="etage" placeholder="Étage" maxlength="6" size="6"><input type="text" name="numero" id="numero" placeholder="Numéro" maxlength="6" size="6"><br>
-        <label for="bat">Corp de metier:</label><input type="text" ><br>
-        <label for="bat">Matériau:</label><input type="text" ><label for="bat">  Matériel:</label><input type="text" ><br>
+        <label for="bat">localisation: </label><input type="text" name="bat" id="bat" placeholder="Bat." maxlength="6" size="6" ><input type="text" name="entree" id="entree" placeholder="Entrée" maxlength="6" size="6"><input type="text" name="cage" id="cage" placeholder="Cage" maxlength="6" size="6" ><input type="text" name="etage" id="etage" placeholder="Étage" maxlength="6" size="6"><input type="text" name="numero" id="numero" placeholder="Numéro" maxlength="6" size="6"><br>
+        <label for="bat">Corp de metier: </label><input type="text" ><br>
+        <label for="bat">Matériau: </label><input type="text" ><label for="bat">  Matériel: </label><input type="text" ><br>
         <label for="fileToUpload">Photo:</label><input name="fileToUpload" id="fileToUpload" type="file"  >
-        <label for="description">Description:</label><input  name="description" id="description" type="text" size="30" ><br>
-        <label for="bat">Longueur:</label><input type="text" max length="6" size="6" placeholder="cm." ><label for="bat">  Largeur:</label><input type="text" maxlength="6" size="6" placeholder="cm."><label for="bat">  Hauteur:</label><input type="text" maxlength="6" size="6" placeholder="cm."><br>
+        <label for="description">Description: </label><input  name="description" id="description" type="text" size="50" ><br>
+        <label for="bat">Longueur: </label><input type="text" max length="6" size="6" placeholder="cm." ><label for="bat">  Largeur: </label><input type="text" maxlength="6" size="6" placeholder="cm."><label for="bat">  Hauteur: </label><input type="text" maxlength="6" size="6" placeholder="cm."><br>
         <label for="bat">Quantité: </label><input type="number" >
 <div class="panel panel-default">
   <div class="panel-body">
-        <label for="bat">Qualité:</label><input type="range"  style="width: 200px" name="qualite" min="0" max="5" ><input type="text" ><br>
+        <label for="bat">Qualité:</label><input type="range"  style="width: 200px" name="qualite" min="0" max="5" ><label for="bat">commentaire: </label><input type="text" ><br>
  </div>
 </div>        
 <div class="panel panel-default">
   <div class="panel-body">
-        <label for="bat">Risque dépose:</label><input type="range" style="width: 200px" name="risque" min="0" max="5" ><input type="text" ><br>
+        <label for="bat">Risque dépose:</label><input type="range" style="width: 200px" name="risque" min="0" max="5" ><label for="bat">commentaire: </label><input type="text" ><br>
  </div>
 </div>        
 <div class="panel panel-default">
   <div class="panel-body">
-        <label for="bat">Compléxité traitement:</label></label><input type="range" style="width: 200px" name="tratement" min="0" max="5" ><input type="text" ><br>
+        <label for="bat">Compléxité traitement:</label><input type="range" style="width: 200px" name="tratement" min="0" max="5" ><label for="bat">commentaire: </label><input type="text" ><br>
  </div>
 </div>
         
 
 <div class="panel panel-default">
   <div class="panel-body">
-    <label for="bat">Compléxité conditionnement et stockage:</label><input type="range" style="width: 200px" name="stockage" min="0" max="5" ><input type="text" ><br>
+    <label for="bat">Compléxité conditionnement et stockage:</label><input type="range" style="width: 200px" name="stockage" min="0" max="5" ><label for="bat">commentaire: </label><input type="text" ><br>
   </div>
 </div>
 
-        <label for="bat">Remarques: </label><input type="text" size="30" ><br>
+        <label for="bat">Remarques: </label><input type="text" size="50" ><br>
       
       </div>
         
