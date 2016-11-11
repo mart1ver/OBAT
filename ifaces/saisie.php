b1fc22f0331fab37546b6b665113ab7b59155f4e
martin vert <mart1ver@gmail.com>
2016-11-11 19:04:23 +0100

    inssertion spot

--- ifaces/saisie.php
+++ ifaces/saisie.php
@@ -243,7 +243,7 @@ var mousePositionControl = new ol.control.MousePosition({
         source: source,
         style: new ol.style.Style({
           fill: new ol.style.Fill({
-            color: 'rgba(255, 255, 0, 1.2)'
+            color: 'rgba(40, 255, 0, 1.2)'
           }),
           stroke: new ol.style.Stroke({
             color: '#ccff33',
