6e4dea4786f27a7d267bc316c1084c219e5ee7dc
martin vert <mart1ver@gmail.com>
2016-11-13 18:34:58 +0100

    inssertion spotA

--- moteur/saisie_post.php
+++ moteur/saisie_post.php
@@ -18,7 +18,7 @@
 
 // Insertion du post à l'aide d'une requête préparée
     $req = $bdd->prepare('INSERT INTO spots (coordos, bat_entree_cage_etage_numero, id_materiaux, id_objets,description,qantite,encombrement,qualite,risque_depose,complexite_traitement,complexite_stockage,remarque,comm_risque_depose,   comm_complexite_traitement,comm_complexite_stockage,comm_qualite) VALUES(?, ?, ?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)');
-    $req->execute(array($_POST['coordos'], $_POST['bat']."|".$_POST['entree']."|".$_POST['cage']."|".$_POST['etage']."|".$_POST['numero'] , $_POST['materiaux'],$_POST['objets'],$_POST['description'],$_POST['quantite'],$_POST['longueur']."|".$_POST['largeur']."|".$_POST['hauteur'],$_POST['qualite'],$_POST['risque'],$_POST['traitement'],$_POST['stockage'],$_POST['remarques'],$_POST['c1'],$_POST['c2'],$_POST['c3'],$_POST['c4']));
+    $req->execute(array($_POST['coordos'], $_POST['bat']."|".$_POST['entree']."|".$_POST['cage']."|".$_POST['etage']."|".$_POST['numero'] , $_POST['materiaux'],$_POST['objet'],$_POST['description'],$_POST['quantite'],$_POST['longueur']."|".$_POST['largeur']."|".$_POST['hauteur'],$_POST['qualite'],$_POST['risque'],$_POST['traitement'],$_POST['stockage'],$_POST['remarques'],$_POST['c1'],$_POST['c2'],$_POST['c3'],$_POST['c4']));
    
     $req->closeCursor();
 
