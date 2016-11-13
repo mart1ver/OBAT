a218c40caad39deaee41ff3f29fa56c33b85682b
martin vert <mart1ver@gmail.com>
2016-11-13 23:15:55 +0100

    inssertion spotA

--- ifaces/saisie.php
+++ ifaces/saisie.php
@@ -302,21 +302,21 @@ var source = new ol.source.Vector({wrapX: false});
       rome.setStyle(new ol.style.Style({
         image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
       
-          src: '../images/images/dot.png'
+          src: '../images/folios/dot.png'
         }))
       }));
 
       london.setStyle(new ol.style.Style({
         image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
         
-          src: '../images/images/dot.png'
+          src: '../images/folios/dot.png'
         }))
       }));
 
       madrid.setStyle(new ol.style.Style({
         image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
           
-          src: '../images/images/dot.png'
+          src: '../images/folios/dot.png'
         }))
       }));
 
