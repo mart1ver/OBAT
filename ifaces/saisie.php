172e5da70e4a5e1c6105792556e2c787118ef17e
martin vert <mart1ver@gmail.com>
2016-11-14 14:53:52 +0100

    inssertion spotA

--- ifaces/saisie.php
+++ ifaces/saisie.php
@@ -325,8 +325,7 @@ var mousePositionControl = new ol.control.MousePosition({
 
       var vectorLayer = new ol.layer.Vector({
         source: vectorSource,
-         projection: projection,
-      extent: extent
+       
        
       });
 /**
@@ -343,7 +342,8 @@ var mousePositionControl = new ol.control.MousePosition({
         source: new ol.source.ImageStatic({
          attributions: '<a href="http://www.emancipo.tk">© Martin VERT</a>',
               url: '<?php echo("../images/folios/".$nom_fichier_folio); ?>',
-           
+              projection: projection,
+              imageExtent: extent 
           
         })
       });
