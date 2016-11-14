2b87557bf4a727b5a319eab9a655624de3ae3de8
martin vert <mart1ver@gmail.com>
2016-11-14 12:12:53 +0100

    inssertion spotA

--- ifaces/saisie.php
+++ ifaces/saisie.php
@@ -325,7 +325,7 @@ var mousePositionControl = new ol.control.MousePosition({
 
       var vectorLayer = new ol.layer.Vector({
         source: vectorSource,
-        
+
        
       });
 
@@ -349,7 +349,7 @@ var mousePositionControl = new ol.control.MousePosition({
         target: document.getElementById('map'),
         logo: false,
          view: new ol.View({
-          center: ol.proj.fromLonLat([2.896372, 44.60240]),
+          center: ol.proj.fromLonLat([1268, 522]),
           zoom: 3
         })
       });
