b93bb25d70933e74fa9919f2ed5526c92eda9def
martin vert <mart1ver@gmail.com>
2016-11-14 14:30:11 +0100

    inssertion spotA

--- ifaces/saisie.php
+++ ifaces/saisie.php
@@ -341,7 +341,8 @@ var mousePositionControl = new ol.control.MousePosition({
       var rasterLayer = new ol.layer.Tile({
         source: new ol.source.TileJSON({
           url: 'https://api.tiles.mapbox.com/v3/mapbox.geography-class.json?secure',
-          imageExtent: extent
+          imageExtent: extent,
+          projection: projection
         })
       });
 
