9270c3b2b816c46f70615e0185ffcd7cbac2ee9a
martin vert <mart1ver@gmail.com>
2016-12-04 18:05:47 +0100

    coef. notes

--- ifaces/notes.php
+++ ifaces/notes.php
@@ -5,6 +5,27 @@ require_once('../moteur/dbconfig.php');
 //Oressource 2014, formulaire de référencement des conventions avec les partenaires de la structure
 //Simple formulaire de saisie , liste des conventions déjà référencées et possibilité de les cacher à l'utilisateur ou de modifier les données
 //
+
+//on extrait les coefs...
+ $req2 = $bdd->prepare("SELECT *  FROM notes  ");
+    $req2->execute();
+    
+ 
+           // On affiche chaque entree une à une
+           while ($donnees2 = $req2->fetch())
+           {
+$coef_qualité = $donnees2['coef_qualité'];
+$coef_depose = $donnees2['coef_depose'];
+$coef_traitement = $donnees2['coef_traitement'];
+$coef_stockage = $donnees2['coef_stockage'];
+
+           }
+
+
+
+ $req2->closeCursor(); // Termine le traitement de la requête 
+
+
 ?>
 
     <div class="container">
@@ -14,10 +35,10 @@ require_once('../moteur/dbconfig.php');
       <div class="panel-body">
         
           <form action="../moteur/edition_notes_post.php" method="post">
-  <div class="row"><label for="note1">Coef. qualité:</label> <input name="note1" id="note1" type="number" value ="" required autofocus></div>
-  <div class="row"><label for="note2">Coef. risque de dépose:</label> <input name="note2" id="note2" type="number" value ="" required ></div>
-  <div class="row"><label for="note3">Coef. complexité de traitement:</label> <input name="note3" id="note3" type="number" value ="" required ></div>
-  <div class="row"><label for="note4">Coef. complexité conditionnement et stockage:</label> <input name="note4" id="note4" type="number" value ="" required ></div> 
+  <div class="row"><label for="note1">Coef. qualité:</label> <input name="note1" id="note1" type="number" value ="<?php echo($coef_qualité)?>" required autofocus></div>
+  <div class="row"><label for="note2">Coef. risque de dépose:</label> <input name="note2" id="note2" type="number" value ="<?php echo($coef_depose)?>" required ></div>
+  <div class="row"><label for="note3">Coef. complexité de traitement:</label> <input name="note3" id="note3" type="number" value ="<?php echo($coef_traitement)?>" required ></div>
+  <div class="row"><label for="note4">Coef. complexité conditionnement et stockage:</label> <input name="note4" id="note4" type="number" value ="<?php echo($coef_stockage)?>" required ></div> 
   <br>
  
   <br>
