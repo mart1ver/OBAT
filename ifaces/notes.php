3cb61fca16361c15241327f5e6ba4c36b23dd8be
martin vert <mart1ver@gmail.com>
2016-12-04 17:59:03 +0100

    fonction de recherche

--- ifaces/notes.php
+++ ifaces/notes.php
@@ -12,14 +12,14 @@ require_once('../moteur/dbconfig.php');
          <div class="panel-heading">Gérez ici les coef. de chaque note.</div>
          <p>Permet de de configurer la note finale en fonction du poid  </p>
       <div class="panel-body">
-        <div class="row">
+        
           <form action="../moteur/edition_notes_post.php" method="post">
-  <div class="col-md-1"><label for="note1">Coef. qualité:</label> <input name="note1" id="note1" type="number" value ="" required autofocus></div>
-  <div class="col-md-1"><label for="note2">Coef. risque de dépose:</label> <input name="note2" id="note2" type="number" value ="" required ></div>
-  <div class="col-md-1"><label for="note3">Coef. complexité de traitement:</label> <input name="note3" id="note3" type="number" value ="" required ></div>
-  <div class="col-md-1"><label for="note4">Coef. complexité conditionnement et stockage:</label> <input name="note4" id="note4" type="number" value ="" required ></div> 
+  <div class="row"><label for="note1">Coef. qualité:</label> <input name="note1" id="note1" type="number" value ="" required autofocus></div>
+  <div class="row"><label for="note2">Coef. risque de dépose:</label> <input name="note2" id="note2" type="number" value ="" required ></div>
+  <div class="row"><label for="note3">Coef. complexité de traitement:</label> <input name="note3" id="note3" type="number" value ="" required ></div>
+  <div class="row"><label for="note4">Coef. complexité conditionnement et stockage:</label> <input name="note4" id="note4" type="number" value ="" required ></div> 
   <br>
-  </div>
+ 
   <br>
 
 
