8accdfa8af1efe63165a57b4310752ba6c8c3f77
Vert martin <mart1ver@gmail.com>
2017-03-09 14:55:32 +0100

    bug infobulle

--- ifaces/recherche.php
+++ ifaces/recherche.php
@@ -679,12 +679,13 @@ $req->closeCursor(); // Termine le traitement de la requête
 
     // display popup on click
     map.on('click', function (evt) {
+        $(element).popover('destroy');
         var feature = map.forEachFeatureAtPixel(evt.pixel,
                 function (feature) {
                     return feature;
                 });
         if (feature) {
-              $(element).popover('destroy');
+              
             var coordinates = feature.getGeometry().getCoordinates();
             popup.setPosition(coordinates);
             $(element).popover({
