627c9623db752a798b27bb22cc0b2341a75e3d2c
Vert martin <mart1ver@gmail.com>
2017-03-09 22:52:33 +0100

    ..

--- ifaces/recherche.php
+++ ifaces/recherche.php
@@ -700,7 +700,7 @@ $req->closeCursor(); // Termine le traitement de la requête
             var coordinates = <?php echo('a'.$_GET["pid"]); ?>.getGeometry().getCoordinates();
             
             popup.setPosition(coordinates);
-             popup.setOffset([-50, -70]);
+             popup.setOffset([-52, -73]);
 
             $(element).popover({
 
